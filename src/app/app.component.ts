021d3b0bb40953a649a371c162eb4f2c5e872aeb
kartalahmetbugra <kartalahmetbugra@gmail.com>
2025-03-19 23:56:44 +0300

    v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Action } from 'rxjs/internal/scheduler/Action';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'todoApp';
+  person = 'ABK';
+  items = [
+    { description: 'Kahvaltı', action: 'No' },
+    { description: 'Sinema', action: 'No' },
+    { description: 'Ders', action: 'No' },
+    { description: 'Spor', action: 'No' },
+  ];
 }
